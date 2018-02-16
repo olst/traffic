cee2fddbc2999c9850a26b47fe75ebd9432f143d
Borys Hulii <8tomat8@gmail.com>
2018-02-16 09:00:17 +0200

    Added changes from review

--- traffic.go
+++ traffic.go
@@ -62,12 +62,14 @@ func (c *Counter) Increment() int {
 // Car ...
 type Car struct {
 	Name    string
+	Dest    int
+	Origin  int
 	entered time.Time
 }
 
 // NewCar - creates a car
 func NewCar() Car {
-	car := Car{Name: "car-" + strconv.Itoa(counter.Increment())}
+	car := Car{Name: "car-" + strconv.Itoa(counter.Increment()), Dest: rand.Intn(3)}
 	//car.Name = "car-" + strconv.Itoa(int(atomic.AddInt32(&counter2, 1)))
 	return car
 }
@@ -80,54 +82,56 @@ func getRandomInt(n int) int {
 func trafficChecker(circle chan Car, toExit chan Car) {
 	for car := range circle {
 		go func(car Car) {
+			// (car.Dest -car.Origin) * time.Second
+			// TODO: Depends on time
 			time.Sleep(1 * time.Second)
 			toExit <- car
 		}(car)
 	}
 }
 
-func inputCar(circle chan Car, roadName string, every time.Duration) {
+func inputCar(roadName string, every time.Duration) Car {
 	car := NewCar()
 	car.entered = time.Now()
+	car.Origin = 2
 	fmt.Printf("%s comes from %s Input Road\n", car.Name, roadName)
-	circle <- car
 	time.Sleep(every)
+	return car
 }
 
 // FirstInputRoad generates random number (0 to 5) of cars per second
-func FirstInputRoad(cirlce chan Car) {
+func FirstInputRoad(circle chan Car) {
 	n := getRandomInt(5)
 	for i := 0; i < n; i++ {
-		inputCar(cirlce, "1st", 0)
+		circle <- inputCar("1st", 0)
 	}
 }
 
 // SecondInputRoad generates 1 car each second
 func SecondInputRoad(circle chan Car) {
 	for {
-		inputCar(circle, "2nd", 1*time.Second)
+		circle <- inputCar("2nd", 1*time.Second)
 	}
 }
 
 // ThirdInputRoad generates a car each two seconds
-func ThirdInputRoad(cirlce chan Car) {
+func ThirdInputRoad(circle chan Car) {
 	for {
-		inputCar(cirlce, "3rd", 2*time.Second)
+		circle <- inputCar("3rd", 2*time.Second)
 	}
 }
 
 // FourthInputRoad generates 10 cars per second
-func FourthInputRoad(cirlce chan Car) {
+func FourthInputRoad(circle chan Car) {
 	for {
 		for i := 0; i < 10; i++ {
-			inputCar(cirlce, "4th", 0)
+			circle <- inputCar("4th", 0)
 		}
 		time.Sleep(1 * time.Second)
 	}
 }
 
-func outputCar(exit chan Car, roadName string, every time.Duration) {
-	car := <-exit
+func outputCar(car Car, roadName string, every time.Duration) {
 	fmt.Printf("%s is exiting into %s Output Road, time: %s\n",
 		car.Name, roadName, time.Now().Sub(car.entered))
 	time.Sleep(every)
@@ -135,10 +139,11 @@ func outputCar(exit chan Car, roadName string, every time.Duration) {
 
 // FirstOutputRoad receives random number (0 to 5) of cars per second
 func FirstOutputRoad(exit chan Car) {
+	// TODO: Use time.NewTicker() . Close()
 	n := getRandomInt(5)
 	for {
 		for i := 0; i < n; i++ {
-			outputCar(exit, "1st", 0)
+			outputCar(<-exit, "1st", 0)
 		}
 		time.Sleep(1 * time.Second)
 	}
@@ -146,15 +151,16 @@ func FirstOutputRoad(exit chan Car) {
 
 // SecondOutputRoad receives one car each second
 func SecondOutputRoad(exit chan Car) {
+	// TODO: Graceful shutdown (context/chan/break...)
 	for {
-		outputCar(exit, "2nd", 1*time.Second)
+		outputCar(<-exit, "2nd", 1*time.Second)
 	}
 }
 
 // ThirdOutputRoad receives a car each hour
 func ThirdOutputRoad(exit chan Car) {
 	for {
-		outputCar(exit, "3rd", 1*time.Hour)
+		outputCar(<-exit, "3rd", 1*time.Hour)
 	}
 }
 
@@ -162,7 +168,7 @@ func ThirdOutputRoad(exit chan Car) {
 func FourthOutputRoad(exit chan Car) {
 	for {
 		for i := 0; i < 10; i++ {
-			outputCar(exit, "4th", 0)
+			outputCar(<-exit, "4th", 0)
 		}
 		time.Sleep(1 * time.Second)
 	}
